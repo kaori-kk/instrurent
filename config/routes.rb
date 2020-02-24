a252ccc3cde3fda8c90a58b669c91b560a0e6be1
kaori kohama <kkaori131415@gmail.com>
2020-02-24 17:58:43 +0100

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,19 @@
 Rails.application.routes.draw do
-  root to: 'pages#home'
+  root to: 'instruments#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :instruments
+
+  resources :users do
+    collection do
+      get 'dashboard#all_requests'
+      get 'dashboard#my_requests'
+    end
+  end
+
+  # resources :users, only: [:new, :create]
+
+  # resources :instruments do
+  #   resources :users, only: [:show]
+  # end
+
 end
