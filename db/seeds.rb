4bdaccb2febcdf1a6de67ffe8dbf31eb9d81d0ec
Julia Mackamul <julia.mackamul@gmail.com>
2020-02-25 18:13:26 +0100

    instrument-view-further-work

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+# instruments = instrument.create([{name: 'Guitar', price: '15', description: 'Like new', user_id: '1'}])
+
+
+puts "cleaning db"
+Request.destroy_all
+Instrument.destroy_all
+guitar = Instrument.new({ name: 'Guitar', price: 20, description: 'Nice vintage guitar'})
+guitar.user = User.last
+request = Request.new({user: User.last, instrument: guitar, accepted: false, start_date: Date.parse('17/9/2015'), end_date: Date.parse('19/9/2015')})
+request.user = User.last
+guitar.save
+request.save
+puts "guitar msgs"
+puts guitar.errors.full_messages
+puts "request msgs"
+puts request.errors.full_messages
+puts "you have #{Instrument.count} instruments and #{Request.count} requests"
