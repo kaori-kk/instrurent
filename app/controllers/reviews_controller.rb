884d7f61f0e7e6c9ff985578be7d493b762c05f6
Julia Mackamul <julia.mackamul@gmail.com>
2020-02-27 17:17:55 +0100

    Added review views

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,51 @@
 class ReviewsController < ApplicationController
+
+  def new
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(instrument_params)
+    @review.user = current_user
+    if @review.save
+      redirect_to instruments_path(@instrument)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @review = Review.find(params[:id])
+    require_owner!(@review)
+  end
+
+  def update
+    @review = Review.find(params[:id])
+    require_owner!(@review)
+
+    @review.update(review_params)
+    redirect_to instrument_path(@instrument)
+  end
+
+  def destroy
+    @review = Instrument.find(params[:id])
+    require_owner!(@review)
+
+    @review.destroy
+    redirect_to instruments_path
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:request_id, :user_stars, :user:review, :intrument_stars, :instrument_review)
+  end
+
+  def require_owner!(review)
+    if (current_user != instrument.user)
+      flash[:notice] = 'You are not authorized to do this.'
+      redirect_to instruments_path
+    end
+  end
+
 end
