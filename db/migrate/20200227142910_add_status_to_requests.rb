2563393a10134f073199be9f16df15322830ff48
Abel van Esseveldt <abelvess@gmail.com>
2020-02-27 15:38:10 +0100

    Made the migration

--- db/migrate/20200227142910_add_status_to_requests.rb
+++ db/migrate/20200227142910_add_status_to_requests.rb
@@ -1,4 +1,5 @@
 class AddStatusToRequests < ActiveRecord::Migration[5.2]
   def change
-    add_column :requests, :status, :string, default: "pending"
+    add_column :requests, :status, :string, default: 'pending'
+  end
 end
